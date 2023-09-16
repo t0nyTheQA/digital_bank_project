cf73bdb00b7d6a80298d646b96d26485e2336b58
Anton <antonytor.live@gmail.com>
2023-09-16 09:44:34 -0400

    'added mock data and browserHelper classes, and added dependency for java faker'

---
+++ src/test/java/ui/Utilities/BrowserHelper.java
@@ -0,0 +1,28 @@
+package ui.Utilities;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class BrowserHelper {
+
+    //this class is for methods with specific things` we do, and to eliminate same code we make methods.
+
+
+    public static WebElement waitForVisibilityOfElement(WebDriver driver, WebElement element, int timeToWaitInSeconds) {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(timeToWaitInSeconds));
+
+        return wait.until(ExpectedConditions.visibilityOf(element));
+    }
+
+    public static WebElement waitUntilClickableAndClickElement(WebDriver driver,WebElement element, int timeToWaitInSeconds) {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(timeToWaitInSeconds));
+        WebElement clickableElement = wait.until(ExpectedConditions.elementToBeClickable(element));
+        clickableElement.click();
+
+        return clickableElement;
+    }
+}
